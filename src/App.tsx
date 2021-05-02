5ce060875d3d894b9bf2f1362682318d9838d40c
Finn Pauls <mbp@pauls.dev>
2021-05-02 20:33:02 +0200

    Add ArtBoards and move them around
    Used a temporary useDrag hook to just take care of dragging. This is
    buggy though so should be replaced soon.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
+import useDrag from './useDrag'
+import {nanoid} from 'nanoid'
 
-export default function App() {
-  return <Main>a nice new project</Main>
+type ArtBoard = {
+  id: string
 }
 
+export default function App() {
+  const [artboards, setArtboards] = useState<ArtBoard[]>([])
+
+  return (
+    <Main>
+      <button onClick={addArtBoard}>Add artboard</button>
+      {artboards.map((artboard) => (
+        <ArtBoardView key={artboard.id} />
+      ))}
+    </Main>
+  )
+
+  function addArtBoard() {
+    setArtboards((artboards) => [...artboards, {id: nanoid()}])
+  }
+}
+
+function ArtBoardView() {
+  const ref = useDrag()
+  return <ArtBoardContainer ref={ref} />
+}
+
+const ArtBoardContainer = styled.div`
+  width: 128px;
+  height: 128px;
+  position: absolute;
+  background-color: white;
+  top: 0px;
+  left: 0px;
+`
+
 const Main = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  position: relative;
   height: 100%;
 
-  background-color: #eeeee4;
+  background-color: #e5e5e5;
   color: #242422;
-  font-size: 75px;
 `
